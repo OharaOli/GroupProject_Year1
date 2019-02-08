44e13a9857ba44d46e0745aa787d0d13c4d38a70
Manne Hedlund <manne.hedlund@student.manchester.ac.uk>
2019-02-08 20:53:57 +0000

    adding index page for player along with js file

--- host/temp.js
+++ host/temp.js
@@ -8,7 +8,18 @@ if (hostButton) {
 	});
 }*/
 
+var numberOfConnectedPlayers = 0;
+var introUpdateTime = 200;
+
 // A function which returns the value of the input field with id: quiz-code-host.
 function selectHostID() {
 	return document.getElementById("quiz-code-host").value;
 }
+
+function displayPlayers() {
+  setInterval(function () {
+    for (screenName in Players) {
+    }
+  }, introUpdateTime);
+}
+
