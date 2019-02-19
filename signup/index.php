431c0a8eaaf0d02eccda32aa27aa59df06beba1d
Romans Kuznecovs <romans.kuznecovs@student.manchester.ac.uk>
2019-02-19 15:23:19 +0000

    The sign up index.php file has now been commented

--- signup/index.php
+++ signup/index.php
@@ -1,9 +1,12 @@
 <?php
+//starts session
 session_start();
 
+//Including the files
 require_once('../misc/config.inc.php');
 require_once("../misc/sqlFunctions.php");
 
+//opening  connection to database 
 $mysqli = new mysqli($database_host, $database_user,
                                         $database_pass, $group_dbname);
 
@@ -15,15 +18,18 @@ $signupErrorMessage= "";
 $username = "";
 $signedUp = false;
 
+//If the webpage has been refreshed with details submitted from a form
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
 {
+    //Set username variable to the username sent friom the form
     $username = $_POST["username"];
+
+    //Username must not be empty
   	if (empty($username)) 
         $signupErrorMessage = "You must enter a username."; 
 	else 
   	{
-        $username = trim($username);
-        // check if name only contains letters, numbers and whitespace
+        // check if name only contains letters and numbers and it is not JUST numbers and it is between 3 and 16 characters long 
         //Regex taken from https://www.w3schools.com/php/php_form_url_email.asp
         if (!preg_match("/^[a-zA-Z0-9]*$/",$username) or preg_match("/^[0-9]*$/",$username) or strlen($username) < 3 or strlen($username) > 16) 
         {
@@ -31,24 +37,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
         }//if
         else
         {
+            //Try to fetch the username from the database and if it returns something other than an empty list, that means username exists
             $result = mysqli_fetch_assoc(sqlWithResult1($mysqli, "SELECT username FROM users WHERE username=(?);", $username));
+
             if ($result != '')
                 $signupErrorMessage  = "The username already exists.";
             else
             {
+                //Make sure the password is between 8 and 25 characters
                 if (strlen($_POST["password"]) < 8 or strlen($_POST["password"]) > 25)
                     $signupErrorMessage = "The password length must be between 8 and 25 characters.";
 
+                //Make sure that the password and repeated password are the same
                 else if ($_POST["password"] == $_POST["repeatedPass"])
                 {
+                    //Encrypt password
                     $hashedPass = password_hash($_POST["password"], PASSWORD_DEFAULT);
-                    //Preventing SQL injections - commands running separately
+                    //Insert the username and password into database
                     sqlWithoutResult2($mysqli, "INSERT INTO  users (username, password) VALUES (?, ?);", $username, $hashedPass);
+                    //Sign up has been successful
                     $signedUp = true;
                 }//else if
+
                 else
                     $signupErrorMessage = "The passwords do not match.";
-       
             }//else
         }//else
   	}//else
@@ -86,14 +98,13 @@ Re-Enter Password: <input type="password" name="repeatedPass" value="">
 <br><br>
 
 <?php 
+//If this is after the details have been sent, and sign up has failed
 if ($_SERVER["REQUEST_METHOD"] == "POST" and !$signedUp)
+    //Display the error message
     echo $signupErrorMessage;
  ?>
 <br>
 <input type="submit" name = "submit" value = "Sign up">
 </form>
-
-
-
 </body>
 </html>
