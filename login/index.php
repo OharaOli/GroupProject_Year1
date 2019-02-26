83c55e5bbe36f303da5e97b5fba625d436c61f5e
Praeveen Ganesharaja <praeveen.ganesharaja@student.manchester.ac.uk>
2019-02-26 15:09:44 +0000

    changed the databse connected to Mannes db

--- login/index.php
+++ login/index.php
@@ -5,7 +5,7 @@ require_once('../misc/config.inc.php');
 require_once('../misc/sqlFunctions.php');
 
 $mysqli = new mysqli($database_host, $database_user,
-                                        $database_pass, $group_dbname);
+                                        $database_pass, $group_dbnames[0]);
 
 // Check for errors before doing anything else
 if($mysqli -> connect_error) 
