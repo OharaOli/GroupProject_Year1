8688b169cc5b0c68afaf98908243808475d70f6d
Praeveen Ganesharaja <praeveen.ganesharaja@student.manchester.ac.uk>
2019-03-15 14:25:03 +0000

    Added the license in the header

--- login/index.php
+++ login/index.php
@@ -82,6 +82,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 
 ?>
 <!DOCTYPE html>
+<!--
+	Landed by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
 <html lang="en">
 <head>
 	<title>Login</title>
