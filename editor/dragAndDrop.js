a4010583f00e61dcb80fc87d0fedb480fc13c349
Eu-Bin KIM <eu-bin.kim@student.manchester.ac.uk>
2019-03-20 12:23:45 +0000

    fix

--- editor/dragAndDrop.js
+++ editor/dragAndDrop.js
@@ -19,7 +19,7 @@ function createDragConfigSub()
     //you first have to get the parentX
     var parentX = draggedElement.getAttribute('data-x');
     updateSubQIndex(parentX);
-    updateRootQIndex();
+    updateRootQIndexDragAndDrop();
   })
 
   return dragConfig;
